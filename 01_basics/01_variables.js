ba321272d6c54dba12088b5fc1a0912b5e36e3c4
Ujjawal Kant <ukant864@gmail.com>
2025-06-29 04:46:03 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountID = 144121
+let accountEmail = "ukks@google.com"
+var accountPassword = "123456"
+accountCity = "bihar"
+let accountstate;
+
+//accountID = 2 not allowed change in accountID
+
+{
+
+
+}
+
+accountEmail = "htre@google.com"
+accountPassword = "7894"
+accountCity = "jaipur"
+
+console.log(accountID);
+/*
+ prefer not to use var
+*/
+console.table([accountID ,accountEmail, accountPassword, accountstate,accountCity]);
+
