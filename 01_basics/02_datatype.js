4af5f6cfcefa085673ee8a488c35bd3a1ce38c6e
Ujjawal Kant <ukant864@gmail.com>
2025-06-30 13:31:15 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,33 @@
+"use strict"; //treat all JS code as newer vesion
+
+//alert(3+3) we are using node js not browser
+
+console.log(33+3); console.log("ujjawal")
+
+console.log(3+3)
+
+//if we console ywo times in a singlr line then semicolon is needed otherwise depend on you
+/* code readability should be high*/
+
+//two documentation is good fo JS is MDN and tce39
+
+ let name = "golu"
+ let age = 19
+ let isloggedIn = false
+ let state;
+
+ /* DataType list
+    number => 2 to power 53
+    bigint = for large integer value
+    string => "  " a;ways in inverted comma
+    boolean => true/false
+    null => standalone vaue{ empty }
+    undefined => 
+    symbol  => unique  */
+    
+    console.log(typeof age);//number
+    console.log(typeof null);//object
+    console.log(typeof undefined);//undefined
+    
+    
+    
