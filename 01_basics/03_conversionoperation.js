040e4b5b8e4c84d137b4bf3c350bd9f930022ca1
Ujjawal Kant <ukant864@gmail.com>
2025-07-01 13:30:40 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,33 @@
+let score = null
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/*"33" => 33
+"33abac" => NaN
+true => 1 , false => 0
+*/
+ 
+let isLoggedIn = "Undefined"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+ //1 => true; 0 => false
+ //"" => false
+ //"ujjawl" => true
+ //"undeined" => true
+ 
+
+ let someNumber = 33
+
+ let stringNumber = string(someNumber)
+ console.log(stringNumber);
+
+ 
+ 
