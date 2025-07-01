16a2e30aa922b1868efbfdc66c0eee0e91b433f8
Ujjawal Kant <ukant864@gmail.com>
2025-07-01 14:17:02 +0000

    OPERATION

---
+++ 01_basics/03_operation.js
@@ -0,0 +1,71 @@
+let value = 3
+let negvalue =-value
+//console.log(negvalue);
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(29*29);
+console.log(2/2);
+console.log(2%3);
+console.log(28**2);
+ these ae basic operation */
+
+ let str1 = "golu"
+ let str2 = "hello"
+
+//console.log( str1 + str2);
+//above also a method for direct console
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+ 2 );
+console.log(1  + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2  + "3");
+
+/*12
+12
+12
+122
+33*/
+
+//console.log((3 + 4) * 5 % 3);
+
+console.log(+true);
+//console.log(true+);
+console.log(+"");
+
+let num1 , num2 , num3  = 2 + 2
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
