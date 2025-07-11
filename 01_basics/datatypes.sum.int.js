fbfe8dd4d8f88e0b67d73e4575fee31d95e28658
Ujjawal Kant <ukant864@gmail.com>
2025-07-11 02:25:34 +0000

    DATATYPES INTERVIEW

---
+++ 01_basics/datatypes.sum.int.js
@@ -0,0 +1,18 @@
+// primitive
+//7 types : string, number, boolean, null, undefined, symbol, BigInt
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = symbol ('123') 
+const anotherid = symbol('123')
+
+console.log(id === anotherid);
+
+//non primitive data type/ reference 
+
+// array, objects, function
