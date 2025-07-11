6c1ac7238d3aa608d9480ad5766804a482878bb0
Ujjawal Kant <ukant864@gmail.com>
2025-07-11 02:11:32 +0000

    COMPARISION

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,29 @@
+/*console.log( 2 > 1 );
+console.log( 2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+
+true
+true
+false
+false*/
+
+//console.log("2" > 1);
+//console.log("02"> 1);
+
+//two different data types
+
+/*console.log(null > 0);
+console.log(null >= 0);
+console.log(null == 0);
+console.log(null < 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+  this type of console always put in doubt*/
+
+  //===
+  
+  console.log("2" === 2);
+  
