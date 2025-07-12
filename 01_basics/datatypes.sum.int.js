1fce818bf156da7d0e736e18d5a9b89074101b8e
Ujjawal Kant <ukant864@gmail.com>
2025-07-12 01:52:13 +0000

    DATATYPE SUMMARY

--- 01_basics/datatypes.sum.int.js
+++ 01_basics/datatypes.sum.int.js
@@ -2,17 +2,30 @@
 //7 types : string, number, boolean, null, undefined, symbol, BigInt
 
 const score = 100
-const scorevalue = 100.3
+const scoreValue = 100.3
 
 const isLoggedIn = false
 const outsideTemp = null
 let userEmail;
 
-const id = symbol ('123') 
-const anotherid = symbol('123')
+//const id = symbol('123') 
+//const anotherId = symbol('123')
 
-console.log(id === anotherid);
+//bigNumber = 565565444644445n
+
 
 //non primitive data type/ reference 
 
 // array, objects, function
+
+const heros = [" virat", "sachin"];
+let myobj = {
+name: "ujjawal",
+age: 20,
+}
+
+const myfunction = function(){
+    console.log("hello golu");
+}
+ 
+console.log(typeof bigNumber);
