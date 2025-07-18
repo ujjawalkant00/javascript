3f58ff553a501c4f616c440a88796536f015ce98
Ujjawal Kant <ukant864@gmail.com>
2025-07-18 13:25:10 +0000

    03_object(API)

---
+++ 02_basics/05_object.js
@@ -0,0 +1,22 @@
+const course = {
+    coursename: "javascript",
+    price: "999",
+    coursereader: "ujjawl"
+    }
+
+    //course.coursereader
+
+    const {coursereader:reader} = course
+    //here we can change
+    console.log(reader);
+    
+/*{
+   "name": "ujjawal",
+    "price": "333",
+    "coursename": "js"
+}*/
+[
+    {},
+    {},
+    {}
+]
